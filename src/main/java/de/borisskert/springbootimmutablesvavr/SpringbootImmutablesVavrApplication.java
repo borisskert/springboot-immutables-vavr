5553e9aa105d02035df0a4ba89fd716f300871f5
Boris Skert <boris.skert@gmail.com>
2022-06-03 11:05:49 +0200

    Create project with Spring Initializr

---
+++ src/main/java/de/borisskert/springbootimmutablesvavr/SpringbootImmutablesVavrApplication.java
@@ -0,0 +1,13 @@
+package de.borisskert.springbootimmutablesvavr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootImmutablesVavrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootImmutablesVavrApplication.class, args);
+	}
+
+}
